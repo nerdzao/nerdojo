560008df21e5d4a402df14da1f280006dc265f35
claudineigibin <45268757+claudineigibin@users.noreply.github.com>
2019-09-28 00:29:28 -0300

    Parte 2 do Exercício.
    Falta implementar as sugestões em caso de números ilegíveis

---
+++ ed/#05-27-09-2019/group-02/02 - Banco OCR.py
@@ -0,0 +1,26 @@
+def retornaNumero(string: str):
+    def num(x):
+        if (x == "     |  |"): return 1
+        if (x == " _  _||_ "): return 2
+        if (x == " _  _| _|"): return 3
+        if (x == "   |_|  |"): return 4
+        if (x == " _ |_  _|"): return 5
+        if (x == " _ |_ |_|"): return 6
+        if (x == " _   |  |"): return 7
+        if (x == " _ |_||_|"): return 8
+        if (x == " _ |_| _|"): return 9
+        return "?"
+
+    x = string.split("\n")
+
+    print (x)
+
+    a = ""
+    numero = []
+    for j in range(1, 10):
+        for i in x:
+            a += i[(3 * j) - 3:3 * j]
+        numero.append(num(a))
+        a = ""
+
+    return numero
