307a652e218e8ebe0f4d80453801195f6b39e366
asreal089 <lexx_89@hotmail.com>
2019-09-29 14:15:12 -0300

    removendo classe

--- ed/#05-27-09-2019/group-02/02-valida.py
+++ ed/#05-27-09-2019/group-02/02-valida.py
@@ -1,14 +1,13 @@
 
-class ValidaConta:
-    def valida(digitos):
+def valida(digitos):
 #digitos = [11] * 9
 #digitos = [ 6, 6,4 , 3, 7, 1, 4 , 9, 5]
-        i = 1
-        soma = 0
-        for x in digitos:
-            soma = soma +(i * x)
-            i += 1
+    i = 1
+    soma = 0
+    for x in digitos:
+        soma = soma +(i * x)
+        i += 1
 
-        return bool( soma % 11 == 0)
+    return bool( soma % 11 == 0)
 
-        
+    
